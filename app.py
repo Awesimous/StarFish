d647fd30f4e1651ae5971e9498c7f82956fc2ca3
HomePC <simon.heinken@gmail.com>
2021-06-04 18:12:08 +0200

    adding new functionality to Twitter and YT API

--- app.py
+++ app.py
@@ -6,9 +6,44 @@ import plotly.graph_objects as go
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit_wordcloud as wordcloud
+from geopy import geocoders
+from geopy.geocoders import Nominatim
 
 # df = pd.read_csv('notebooks/streamer_df_25pages')
 # st.write(df.head)
+
+def city_lat_long(city):
+    geolocator = Nominatim(user_agent='myapplication')
+    location = geolocator.geocode(city)
+    latitude = location[1][0]
+    longitude = location[1][1]
+    return latitude, longitude
+
+def country_lat_long(country):
+    geolocator = Nominatim(user_agent='myapplication')
+    location = geolocator.geocode(country)
+    latitude = location[1][0]
+    longitude = location[1][1]
+    return latitude, longitude
+
+test = ['London', 'Bristol', 'New Brunswick']
+
+@st.cache
+def get_map_data(cities):
+    coords = []
+    for city in cities:
+        city_data = city_lat_long(city)
+        coords.append([city_data[0], city_data[1]])
+    return pd.DataFrame(
+            np.array(coords),
+            columns=['lat', 'lon']
+        )
+      
+
+
+df = get_map_data(test)
+
+st.map(df)
 
 words = [
     dict(text="Robinhood", value=16000, color="#b5de2b", country="US", industry="Cryptocurrency"),
