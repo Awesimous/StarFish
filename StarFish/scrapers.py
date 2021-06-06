80c2942e34a839d596d91ca7540273d05f3f3097
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2021-06-06 23:40:24 +0200

    Add subscriber data scraping

--- StarFish/scrapers.py
+++ StarFish/scrapers.py
@@ -3,8 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-import sys
-from pathlib import Path
+from random import uniform
 
 
 def parse_html(url):
@@ -16,39 +15,70 @@ def parse_html(url):
 
     return df
 
-
-if __name__ =='__main__':
-    pages = range(2, int(sys.argv[1]))
-    url = f"https://twitchtracker.com/channels/viewership?page=1"
-
-    # Scrape channel data
+def get_channels_data(n_pages):
+    pages = range(1, int(n_pages) + 1)
     print('Scraping channel information.')
 
-    # First page
-    df = parse_html(url)
-    # Following pages
+    channels_df = pd.DataFrame()
     for page in pages:
-        print(page)
-        new_df = parse_html(f"https://twitchtracker.com/channels/viewership?page={page}")
-        df = df.append(new_df, ignore_index=True)
-        time.sleep(1)
+        print(f'{page}/{n_pages}')
+        # Get channel overviwew data
+        df = parse_html(f"https://twitchtracker.com/channels/viewership?page={page}")
+        channels_df = channels_df.append(df, ignore_index = True)
+        time.sleep(uniform(1,4))
 
-    # Remove and rename columns and rows as appropriate
+    # Remove and rename channel columns and rows as appropriate
     print('Parsing...')
 
-    df.pop('Unnamed: 1')
-    df = df.rename(columns={'Unnamed: 2': 'Username'})
-    df = df.rename(columns={'Unnamed: 0': 'Id'})
-    to_remove = df["Id"].str.contains('ads')
-    df_toremove_index = df[to_remove].index
-    df = df.drop(index=df_toremove_index)
+    channels_df.pop('Unnamed: 1')
+    channels_df = channels_df.rename(columns={'Unnamed: 2': 'Username'})
+    channels_df = channels_df.rename(columns={'Unnamed: 0': 'Id'})
+    to_remove = channels_df["Id"].str.contains('ads')
+    df_toremove_index = channels_df[to_remove].index
+    channels_df = channels_df.drop(index=df_toremove_index)
+
+    return channels_df
 
 
-    # Write the channel data to file
-    df.to_csv(f'data/channels.csv')
-    # Loop over usernames and get their game data
-    for streamer in df['Username'].head(1).to_list():
+def get_subs_data(n_pages):
+    pages = range(1, int(n_pages) + 1)
+    print('Scraping subscriptions information.')
+
+    subs_df = pd.DataFrame()
+    for page in pages:
+        print(f'{page}/{n_pages}')
+        # Get subs page
+        df = parse_html('https://twitchtracker.com/subscribers?page={page}')
+        subs_df = subs_df.append(df, ignore_index = True)
+        time.sleep(uniform(1,4))
+
+
+    return subs_df
+
+
+def get_games_data(channels_data):
+    for i, streamer in enumerate(channels_data['Username'].to_list()):
         url = f"https://twitchtracker.com/{streamer.lower()}/games"
-        game_df = parse_html(url)
+        try:
+            game_df = parse_html(url)
+        except ValueError:
+            print(f'ValueError at {i}: {streamer}')
+            continue
         # Write the data to csv
+        time.sleep(uniform(1,4))
         game_df.to_csv(f'data/games/{streamer}.csv')
+
+
+
+if __name__ =='__main__':
+    # Define the range of channels and subscribers pages to scrape
+    #n_pages = input('How many pages would you like to scrape? ')
+
+    # Write the channel, game, and subs data to file
+    channels = get_channels_data(50)
+    channels.to_csv(f'data/channels.csv')
+    #channels = pd.read_csv('data/channels.csv')
+    get_games_data(channels)
+    get_subs_data(125).to_csv('data/subs.csv')
+
+
