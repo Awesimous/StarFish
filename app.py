dd9c51ab749bd66f6d21eebbc09bbde03b77541d
HomePC <simon.heinken@gmail.com>
2021-06-08 10:24:44 +0200

    some changes to app.py and upload other changes

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ from StarFish.data import GCPFileHandler
 from PIL import Image
 from streamlit.report_thread import get_report_ctx
 from streamlit.server.server import Server
-
+from StarFish.data import GCPFileHandler
 
 # Download dbs on initial setup
-GCPFileHandler('twitch_data/top_streams_2450.csv')\
-    .download_from_gcp('StarFish/data/raw/top_streams_2450.csv')
-
-
+# GCPFileHandler('twitch_data/top_streams_2450.csv')\
+#     .download_from_gcp('StarFish/data/raw/top_streams_2450.csv')
+# stream_df = pd.read_csv('StarFish/data/raw/top_streams_2450.csv')
+# st.dataframe(stream_df)
 st.set_page_config(layout="wide",initial_sidebar_state="expanded")
 
 
@@ -58,8 +58,15 @@ def page_settings(state):
     #display_state_values(state)
     state.games_df = pd.read_csv('StarFish/data/games_clean.csv')
     state.df = pd.read_csv('StarFish/data/streamers_clean.csv')
+    # create a range of 200 users
+    state.target_df = state.df[500:700]
+    state.usernames = state.target_df.username.to_list()
+    #set usernames as index
     state.target_df = state.df[500:700].set_index('username')
+    # drop outliers as they make range setting difficult
     state.target_df = state.target_df.drop('ESL_CSGO')
+    #make a list with all users in the range
+    
     state.max_followers = int(state.target_df["Total Followers"].max())
     state.max_viewer = int(state.target_df['AVG Viewers'].max())
     state.games_temp = ['Just Chatting', 'Grand Theft Auto V',
@@ -79,18 +86,22 @@ def page_settings(state):
     state.target_df = state.target_df[state.target_df['Total Followers'] >= state.follower]
     state.features = st.multiselect('Select feature:', state.target_df.columns)
     state.feature_df = state.target_df[state.features]
-    state.col_sort = st.multiselect('Select feature to sort on:', state.feature_df.columns)
+    if state.features:
+        state.col_sort = st.radio('Select feature to sort on:', state.feature_df.columns)
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+        st.write(f'You selected {state.col_sort}!')
     state.social = st.multiselect('Select social media:', state.social_media)
 
 
 
 def display_state_values(state):
     st.write('Display the 5 best streamers (if there are 5) based on your chosen categories:')
-    state.feature_df = state.feature_df.sort_values(by=[state.col_sort[0]], ascending=False).head()
-
-    st.table(state.feature_df)
-
-    # c1, c2, c3 = st.beta_columns((1, 1, 2))
+    # show dataframe with 5 best streamers based on selection criterias
+    state.feature_df = state.feature_df.sort_values(by=[state.col_sort], ascending=False).head()
+    st.dataframe(state.feature_df)
+    # select one target to specify data on
+    state.target = st.radio('Do you want to look at any streamer in particular?', state.feature_df.index)
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
     # with c1:
     #     expander=st.beta_expander("expand")
@@ -110,11 +121,117 @@ def display_state_values(state):
 
     #         g = st.plotly_chart(fig)
         #c4, c5, c6 = st.beta_columns((1, 1, 2))
+    
+    # prepare data for plotting (import and process)
+    # index streams for one specific user
+    state.stream_df = pd.read_csv('StarFish/data/top_2450.csv')
+    
+    state.target_stream = state.stream_df[state.stream_df['User'] == state.target]
+    st.write(state.target)
+    st.dataframe(state.feature_df.iloc[state.target])
+    # import games_df
+    state.games_df = pd.read_csv('notebooks/CSVs/Games_df')
+    
+    state.target_games_df = state.games_df[state.games_df['User'] == state.target]
+    st.dataframe(state.target_games_df)
+    state.top_games_max_viewers = state.target_games_df.sort_values(by = ['Avg. viewers'], ascending=False)
+    state.top_games_followers_gained = state.target_games_df.sort_values(by =['Followers per hour'], ascending=False)
+    state.top_10_followers_gained = state.top_games_followers_gained.head(10)
+    state.top_10 = state.top_games_max_viewers.head(10)
+    
+    c1, c2, c3 = st.beta_columns((1, 1, 2))
+    
+    #plot the data
+    with c1:
+        expander=st.beta_expander("expand")
+        with expander:
+            fig = make_subplots(rows=1, 
+                                cols=2,
+                                subplot_titles=("Pie 1", "Pie 2"), 
+                                specs=[
+                                [{"type": "domain"}, {"type": "domain"}]
+                                ])
+            fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 1, 1)
+            fig.add_trace(go.Pie(labels=state.top_games_followers_gained["Game"], values=state.top_games_followers_gained['Followers per hour']), 1, 2)
+            fig.update_layout(
+                showlegend=True,
+                uniformtext_minsize=12, 
+                uniformtext_mode='hide')
 
+            st.plotly_chart(fig)
 
+    with c2:
+        expander=st.beta_expander("expand")
+        with expander:
+            fig = make_subplots(rows=1, 
+                                cols=2,
+                                subplot_titles=("Pie 1", "Pie 2"), 
+                                specs=[
+                                [{"type": "domain"}, {"type": "domain"}]
+                                ])
+            fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 1, 1)
+            fig.add_trace(go.Pie(labels=state.top_games_followers_gained["Game"], values=state.top_games_followers_gained['Followers per hour']), 1, 2)
+            fig.update_layout(
+                showlegend=True,
+                uniformtext_minsize=12, 
+                uniformtext_mode='hide')
+
+            st.plotly_chart(fig)
+        
+    with c3:
+        expander=st.beta_expander("expand")
+        with expander:
+            fig = make_subplots(rows=1, 
+                                cols=2,
+                                subplot_titles=("Pie 1", "Pie 2"), 
+                                specs=[
+                                [{"type": "domain"}, {"type": "domain"}]
+                                ])
+            fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 1, 1)
+            fig.add_trace(go.Pie(labels=state.top_games_followers_gained["Game"], values=state.top_games_followers_gained['Followers per hour']), 1, 2)
+            fig.update_layout(
+                showlegend=True,
+                uniformtext_minsize=12, 
+                uniformtext_mode='hide')
+
+            st.plotly_chart(fig)
+    # fig = make_subplots(rows=2, 
+    #                     cols=2,
+    #                     subplot_titles=("Pie 1", "Pie 2", "Pie 3", 'Pie 4'), 
+    #                     specs=[
+    #                     [{"type": "domain"}, {"type": "domain"}],
+    #                     [{"type": "domain"}, {"type": "domain"}]
+    #                     ])
+    # fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 1, 1)
+    # fig.add_trace(go.Pie(labels=state.top_games_followers_gained["Game"], values=state.top_games_followers_gained['Followers per hour']), 1, 2)
+    # #fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 2, 1)
+    # #fig.add_trace(go.Pie(labels=state.top_10_followers_gained["Game"], values=state.top_10_followers_gained['Followers per hour']), 2, 2)
+    # fig.update_layout(
+    #     showlegend=True,
+    #     uniformtext_minsize=12, 
+    #     uniformtext_mode='hide')
+
+    # st.plotly_chart(fig)
+
+    ## Functions to retrieve city lat and long
+    def city_lat_long(city):
+        geolocator = Nominatim(user_agent='myapplication')
+        location = geolocator.geocode(city)
+        latitude = location[1][0]
+        longitude = location[1][1]
+        return latitude, longitude
+    
+    def country_lat_long(country):
+        geolocator = Nominatim(user_agent='myapplication')
+        location = geolocator.geocode(country)
+        latitude = location[1][0]
+        longitude = location[1][1]
+        return latitude, longitude
+    
+    state.test = ['London', 'Bristol', 'New Brunswick']
 
     if st.button("Clear state"):
-        state.clear()
+            state.clear()
 
 
 class _SessionState:
