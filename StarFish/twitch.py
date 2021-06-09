1a07d8160992629bb5adb264e08de824e3503e02
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2021-06-09 11:37:57 +0200

    WIP twitch api tag retrieval

--- StarFish/twitch.py
+++ StarFish/twitch.py
@@ -5,14 +5,12 @@ import sys
 from twitchAPI.twitch import Twitch
 from dotenv import load_dotenv
 import requests
-from twitchAPI.twitch import Twitch
 import pandas as pd
 
 load_dotenv()
 
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-USER_TOKEN = os.environ.get('USER_TOKEN')
 
 
 
@@ -25,18 +23,17 @@ def authorize(CLIENT_ID = CLIENT_ID, CLIENT_SECRET = CLIENT_SECRET):
 
     return token
 
-def request(api_endpoint, headers, params):
+def request(api_endpoint, headers, params = {}):
     '''Return a resource as json'''
     response = requests.get(
         url = api_endpoint, headers = headers, params = params).json()
 
     return response
 
-
 # define functions to handle each endpoint
-def search_channels(display_name):
+def search_channels(headers, display_name):
     '''Get channel id, login name, and language from an exact streamer handle'''
-    API_ENDPOINT = base_url + 'search/channels'
+    API_ENDPOINT = 'https://api.twitch.tv/helix/search/channels'
     params = {
         'query': display_name,
         'first': 1
@@ -45,7 +42,7 @@ def search_channels(display_name):
     return request(API_ENDPOINT, headers, params)['data'][0]
 
 
-def get_channel_teams(channel_id):
+def get_channel_teams(headers, channel_id):
     '''Get the list of teams to which a streamer belongs by streamer id'''
     API_ENDPOINT = base_url + 'teams/channel'
     params = {
@@ -55,7 +52,7 @@ def get_channel_teams(channel_id):
     return request(API_ENDPOINT, headers, params)['data']
 
 
-def get_teams(team_name, team_id):
+def get_teams(headers, team_name, team_id):
     '''Get team membership and status data from team name and team id'''
     API_ENDPOINT = base_url + 'teams'
     params = {
@@ -66,8 +63,8 @@ def get_teams(team_name, team_id):
     return request(API_ENDPOINT, headers, params)
 
 
-def get_stream_tags(channel_id):
-    API_ENDPOINT = base_url + 'streams/tags'
+def get_stream_tags(headers, channel_id):
+    API_ENDPOINT = 'https://api.twitch.tv/helix/streams/tags'
     params = {
         'broadcaster_id': channel_id
         }
@@ -75,7 +72,7 @@ def get_stream_tags(channel_id):
     return request(API_ENDPOINT, headers, params)['data']
 
 
-def get_top_games(first = 100):
+def get_top_games(headers, first = 100):
     '''return the top n games currently viewed on the platform'''
     API_ENDPOINT = base_url + 'games/top'
     params = {
@@ -85,7 +82,7 @@ def get_top_games(first = 100):
     return request(API_ENDPOINT, headers, params)
 
 
-def get_games(game_id, game_name):
+def get_games(headers, game_id, game_name):
     API_ENDPOINT = base_url + 'games'
     params = {
         'id': game_id,
@@ -95,8 +92,8 @@ def get_games(game_id, game_name):
     return request(API_ENDPOINT, headers, params)
 
 
-def get_all_stream_tags(first = 100, after = None, tag_id = None):
-    API_ENDPOINT = base_url + 'tags/streams'
+def get_all_stream_tags(headers, first = 100, after = None, tag_id = None):
+    API_ENDPOINT = 'https://api.twitch.tv/helix/tags/streams'
     if after:
         params = {
             'after': after,
@@ -114,7 +111,8 @@ def get_all_stream_tags(first = 100, after = None, tag_id = None):
 
     return request(API_ENDPOINT, headers, params)
 
-def search_categories(query, first = 100, after = None):
+
+def search_categories(headers, query, first = 100, after = None):
     API_ENDPOINT = base_url + 'search/categories'
     params = {
         'query': query,
@@ -128,30 +126,30 @@ def search_categories(query, first = 100, after = None):
 
 
 # Functions for collection and aggregation
-def get_streamer_data(display_name):
+def get_streamer_data(headers, display_name):
     '''Relate a streamer handle to their id, language, and login name'''
     streamer = {}
 
-    data = search_channels(display_name)
-    streamer['id'] = data['id']
-    streamer['broadcaster login'] = data['broadcaster_login']
-    streamer['broadcaster language'] = data['broadcaster_language']
+    data = search_channels(headers, display_name)
+    streamer['id'] = [data['id']]
+    streamer['broadcaster_login'] = [data['broadcaster_login']]
+    streamer['broadcaster_language'] = [data['broadcaster_language']]
 
-    data = get_stream_tags(streamer['id'])
+    data = get_stream_tags(headers, streamer['id'])
     if data:
         for tag in data:
-            streamer['tag IDs'] = [tag['tag_id'] for tag in data]
+            streamer['tag_ids'] = [tag['tag_id'] for tag in data]
 
 
     return streamer
 
 
-def get_streamer_teams(display_name):
+def get_streamer_teams(headers, display_name):
     '''Return team information for a given streamer'''
-    streamer_id = search_channels(display_name)['id']
+    streamer_id = search_channels(headers, display_name)['id']
     teams = {'streamer_id': streamer_id}
 
-    data = get_channel_teams(streamer_id)
+    data = get_channel_teams(headers, streamer_id)
     if data:
         for i, team in enumerate(data):
             teams[i] = {
@@ -168,11 +166,9 @@ def get_streamer_teams(display_name):
     return teams
 
 
-
-def identify_relevant_teams(display_names):
+def identify_relevant_teams(headers, display_names):
     '''Return a dataframe of teams comprising the streamers passed'''
     pass
-
 
 
 if __name__ == '__main__':
@@ -183,6 +179,8 @@ if __name__ == '__main__':
     # Get the ID of the user accessing the resource
     user_info = twitch.get_users(logins=['Awesimous'])
     user_id = user_info['data'][0]['id']
+
+    USER_TOKEN = authorize()
 
     # Use the token to "authenticate"
     headers = {
@@ -195,6 +193,7 @@ if __name__ == '__main__':
 
     broadcaster_name = sys.argv[1]
 
-    channel = get_streamer_data(broadcaster_name)
-    teams = get_streamer_teams(broadcaster_name)
-    print(channel, '\n\n', teams)
+    channel = get_streamer_data(headers, broadcaster_name)
+    #teams = get_streamer_teams(headers, broadcaster_name)
+    #print(channel, '\n\n', teams)
+    print(channel)
